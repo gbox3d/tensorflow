bba3b3f897594281fa7544f8f59aa69f8f2eca59
gbox3d <gbox3d@gmail.com>
2020-07-06 17:26:57 +0900

    add gpu test code

---
+++ check_gpu.py
@@ -0,0 +1,24 @@
+#%%
+import tensorflow as tf
+
+
+print("버전: ", tf.__version__)
+print("즉시 실행 모드: ", tf.executing_eagerly())
+print("GPU ", "사용 가능" if tf.config.experimental.list_physical_devices("GPU") else "사용 불가능")
+#%%
+tf.test.is_gpu_available(
+    cuda_only=False, min_cuda_compute_capability=None
+)
+
+# %%
+tf.config.list_physical_devices('GPU')
+
+# %%
+tf.test.is_gpu_available(
+    
+)
+
+# %%
+tf.test.is_built_with_cuda()
+
+# %%
